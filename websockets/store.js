e18c8382d0af8f72b4b239a95755df05b3bda132
SimTheFool <simon.sauzede@gmail.com>
2020-01-28 14:28:56 +0100

    fix store.js instance dield declaration

--- websockets/store.js
+++ websockets/store.js
@@ -6,33 +6,33 @@ class Store {
             users: [],
             pairQueue: []
         }
-    }
 
-    dispatch = function(action, afterEffect = () => {})
-    {
-        let result = action(this);
-        afterEffect(result);
-    }
-
-    findUserByWs = function(ws)
-    {
-        return this.state.users.find((user) => {
-            return user.ws === ws;
-        });
-    }
-
-    findUserIndexByWs = function(ws)
-    {
-        return this.state.users.findIndex((user) => {
-            return user.ws === ws;
-        });
-    }
-
-    findUserIndexInPairQueueByWs = function(ws)
-    {
-        return this.state.pairQueue.findIndex((user) => {
-            return user.ws === ws;
-        });
+        this.dispatch = function(action, afterEffect = () => {})
+        {
+            let result = action(this);
+            afterEffect(result);
+        }
+    
+        this.findUserByWs = function(ws)
+        {
+            return this.state.users.find((user) => {
+                return user.ws === ws;
+            });
+        }
+    
+        this.findUserIndexByWs = function(ws)
+        {
+            return this.state.users.findIndex((user) => {
+                return user.ws === ws;
+            });
+        }
+    
+        this.findUserIndexInPairQueueByWs = function(ws)
+        {
+            return this.state.pairQueue.findIndex((user) => {
+                return user.ws === ws;
+            });
+        }
     }
 };
 
